8b703deccca67832c7bd8901d307ca909609b7f2
KyleRattet <krattet@gmail.com>
2015-09-18 21:23:53 -0600

    poster rendering working

--- js/controllers.js
+++ js/controllers.js
@@ -5,14 +5,10 @@ app.controller('myController', function($scope) {
 
 app.controller('MovieController', function($scope, $http) {
 
-  // $scope.greeting = "movie greeting test";
 
   $scope.getMovies = function () {
-    console.log('test');
-    console.log($scope.movieQuery);
     var movie =
   $http.get('http://www.omdbapi.com/?s='+$scope.movieQuery+'&r=json').then(function(data){
-    console.log(data)
     $scope.moviesData = data.data.Search;
   });
   };
@@ -20,13 +16,9 @@ app.controller('MovieController', function($scope, $http) {
 
   //show page when link is clicked
    $scope.showMovie = function () {
-    console.log('test show movie');
-    // $scope.imbd =imbdID
-    console.log(this.movie.imdbID, "this.movie.id");
     $scope.resultsDiv = false;
     $scope.singleMovie = true;
     $http.get('http://www.omdbapi.com/?i='+ this.movie.imdbID+'&plot=full&r=json').success(function(data){
-    console.log(data)
     $scope.movieData = data;
   });
   };
